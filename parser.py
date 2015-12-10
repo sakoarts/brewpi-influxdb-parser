a0a2f317ee3e3c4ffd31fcf125c646ea4eb7a093
Sako Arts <s.arts@student.tue.nl>
2015-12-10 13:23:05 +0100

    Included comments

--- parser.py
+++ parser.py
@@ -6,21 +6,20 @@ import os
 import datetime
 import time
 
+#Reads json file, parses it to lineprotocol, and returns list of lineprotocol strings
 def readFromFile(file):
     filename = file.split(".")[0]
     lineList = []
-
     with open(file) as json_file:
         data = json.load(json_file)
-
         i = 0
         for c in data["rows"]:
-
             datapoint = escapeChar(filename) + " " + parseFromList(c["c"], data["cols"])
             lineList.append(datapoint)
             i += 1
     return lineList
 
+#Reads list of lineprotocol strings and writes them to file
 def writeListToFile(list, filename):
     newfile = filename + "_lineprotocol.txt"
     for x in list:
@@ -29,6 +28,7 @@ def writeListToFile(list, filename):
         writeLineToFile(newfile, x)
     return newfile
 
+#Inserts \ in front of specal characters to escape them form lineprotocol interpeter
 def escapeChar(value):
     value = str(value)
     if " " in value:
@@ -39,10 +39,12 @@ def escapeChar(value):
         value = value.replace("=", "\=")
     return value
 
+#Writes one line to a file
 def writeLineToFile(newfile, line):
     with open(newfile, "ab") as text_file:
         text_file.write(line)
 
+#Parses json datetime format to unixtimestamp, in case of none it retruns 0
 def parseDatetime(s):
     if s is not "none":
         value = datetime.datetime.strptime(s, "Date(%Y,%m,%d,%H,%M,%S)")
@@ -50,6 +52,7 @@ def parseDatetime(s):
         value = 0
     return value
 
+#Parses json number format to float, in case of none it retruns 0
 def parseNumber(s):
     if s is not "none":
         value = s
@@ -59,13 +62,15 @@ def parseNumber(s):
         value = 0
     return value
 
+#Parses json string format to lineprotocol compatible string, in case of none it retruns "null"
 def parseString(s):
     if s is not "none":
         value = '"' + escapeChar(s) + '"'
     else:
-        value = '"' + 'null' + '"'
+        value = '"null"'
     return value
 
+#Parses json value to lineprotocol compatible value
 def parseValue(x, v):
     if x == 'number':
         value = parseNumber(v)
@@ -77,8 +82,8 @@ def parseValue(x, v):
         value = '"' + 'unknownType' + '"'
     return value
 
+#Parses list representing a datapoint to a string compatible with lineprotocol
 def parseFromList(list, cols):
-
     datapoint = ""
     j = 0
     for v in list:
@@ -91,22 +96,23 @@ def parseFromList(list, cols):
                 value = parseValue(type, v["v"])
             except TypeError:
                 value = parseValue(type, "none")
-
             datapoint += (str(id) + "=" + str(value) + str(","))
         j += 1
-
     return(datapoint[:-1] + " " + str(time.mktime(timestamp.timetuple()))[:-2] + "000000000")
 
+#Adds one line to influx database
 def addLineToDB(db, line):
     os.system("curl -i -XPOST 'http://localhost:8086/write?db=" + db + "' --data-binary '" + line + "'")
 
+#Adds file to influx database
 def addFileToDB(db, file):
     os.system("curl -i -XPOST 'http://localhost:8086/write?db=" + db + "' --data-binary '@" + file + "'")
 
+#Adds each line in list to influx database
 def addListToDB(db, list):
     for x in list:
         addLineToDB(db, x)
 
+#Main function
 if __name__ == '__main__':
-    #readFromFile("mashtest-26-11-2015-2015-11-26.json")
     addFileToDB('brewpi', writeListToFile(readFromFile("mashtest-26-11-2015-2015-11-27.json"), "mashtest-26-11-2015-2015-11-27"))
